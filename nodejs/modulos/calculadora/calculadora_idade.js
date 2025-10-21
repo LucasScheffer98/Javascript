712fb4d2d858856dad4515627420cbdc2bc7951f
Lucas Scheffer Behenck <Behencklucas09@gmail.com>
2025-10-21 08:14:29 -0300

    Versão com produtos e camisetas

--- nodejs/modulos/calculadora/calculadora_idade.js
+++ nodejs/modulos/calculadora/calculadora_idade.js
@@ -1,6 +1,4 @@
-function idade(nome, anoNasc, anoAtual){
+export function idade(nome,anoNasc,anoAtual){
     idade = anoAtual - anoNasc;
-    return `Olá, ${nome}! Em ${anoAtual} você terá ${idade} anos.`;
+    return `Olá, ${nome}! Em ${anoAtual} você terá ${idade} anos!`;
 }
-//Exportando a função para que possa ser usada em outros arquivos
-module.exports = { idade };
--- nodejs/modulos/calculadora/modulos/calculadora_idade.js
+++
@@ -1,5 +0,0 @@
-export function idade(nome, anonascimento, anoatual) {
-    idade = anoatual - anonascimento
-    return `Olá, ${nome}! Você tem ${idade} anos! Hihi!`;
-}
-
